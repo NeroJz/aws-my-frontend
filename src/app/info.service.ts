f9e03587894af61bd62b79161ca128e90802142e
KumHoe <nerojz86@gmail.com>
2024-09-17 17:58:13 +0800

    Change info.service.ts

--- src/app/info.service.ts
+++ src/app/info.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class InfoService {
-  endpoint: string = 'http://localhost:3000';
+  endpoint: string = 'http://<MY_BACKEND_URL>:3000';
 
   constructor(private http: HttpClient) {
   }
